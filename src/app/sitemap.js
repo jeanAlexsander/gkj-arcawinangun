c3a07ef9fa1b8ac40fd9371da75101de3b9323d9
Jean Alexsander Caesar <jeanalexander183@gmail.com>
2026-03-07 21:25:00 +0700

    add SEO metadata and sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,28 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://gkj-arcawinangun.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://gkj-arcawinangun.vercel.app/jadwal-ibadah",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://gkj-arcawinangun.vercel.app/pelayanan",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://gkj-arcawinangun.vercel.app/sejarah",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://gkj-arcawinangun.vercel.app/pengumuman",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://gkj-arcawinangun.vercel.app/kontak",
+      lastModified: new Date(),
+    },
+  ];
+}
