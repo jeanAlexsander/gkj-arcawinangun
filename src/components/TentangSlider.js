66073b9530b7d6443b9e4168ef5b4f21791c2167
Jean Alexsander Caesar <jeanalexander183@gmail.com>
2026-04-29 00:08:56 +0700

    fix tentangslider

--- src/components/TentangSlider.js
+++ src/components/TentangSlider.js
@@ -4,10 +4,11 @@ import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 
 const images = [
+  "/gereja_1.jpg",
+  "/gereja_2.jpg",
   "/kegiatan_1.jpg",
   "/kegiatan_2.jpg",
   "/kegiatan_3.jpg",
-  "/gereja.jpg",
 ];
 
 export default function TentangSlider() {
